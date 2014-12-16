cb1e37b5db514a958232a552efccbff593b9292f
Shahrukh A. Khan <iam@shaz3e.com>
2014-12-17 00:34:53 +0500

    comments removed page layouts

--- layouts/3-columns.php
+++ layouts/3-columns.php
@@ -11,13 +11,7 @@
 while ( have_posts() ) : the_post();
 
 	// Include the page content template.
-	get_template_part( 'content', 'page' );
-	
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template();
-	}
-					
+	get_template_part( 'content', 'page' );					
 
 endwhile;
 
