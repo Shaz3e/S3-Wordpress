f863f7e800a9d9f678d316afb53c550503ab53b4
Shahrukh A. Khan <iam@shaz3e.com>
2014-12-17 00:56:53 +0500

    hide admin tool-bar support added

--- functions.php
+++ functions.php
@@ -10,13 +10,20 @@
  */
  
 
+
 include_once("functions/s3_title.php");
 include_once("functions/s3_sidebars.php");
 include_once("functions/s3_wordpress.php");
 include_once("s3tools/s3_theme_options.php");
 
 
-
+/**
+ * Hide admin tool bar for logged-in user front-end
+ * disable this hook to show admin tool bar at top
+ *
+ * @since S3 Framework 1.0
+ */
+add_filter('show_admin_bar', '__return_false');
 
 
 ?>
