8523c1e9383df31e3c588cd9e1e229a563b7cb21
Shahrukh A. Khan <iam@shaz3e.com>
2014-12-25 23:13:26 +0500

    Disable Page Title on front_page and Layout Page
    more description about title here
    https://github.com/Shaz3e/S3-Wordpress/commit/5b3a26659ccabcff0ab7a73c829f6099e0d4ccd8

--- content-page.php
+++ content-page.php
@@ -9,24 +9,34 @@
  
 ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php
-		// Page thumbnail and title.
-		//twentyfourteen_post_thumbnail();
-		//the_title( '<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->' );
-	?>
     
-    <?php if(!is_front_page()): ?>
+    <?php if(is_front_page() || is_page_template('layouts/no-title.php')):
+		
+		/**
+		 * No title on front page (static) or no-title layout page
+		 *
+		 * @since S3 Framework 1.0
+		 */
+        
+	else: ?>
+        
         <div class="page-header">
             <h1 class="page-title"><?php the_title(); ?></h1>
         </div>
+        
     <?php endif; ?>
     
 	<div class="page-content">
 		<?php
 			// page content
 			the_content();
-
-			//edit_post_link( __( 'Edit', 'S3Framework' ), '<span class="edit-link">', '</span>' );
+			
+			/** 
+			 * edit link visisble if user is logged and has permission to edit the page
+			 * 
+			 * @since S3 Framework 1.0
+			 */
+			edit_post_link( __( 'Edit', 'S3Framework' ), '<span class="edit-link">', '</span>' );
 		?>
 	</div><!-- .page-content -->
     
