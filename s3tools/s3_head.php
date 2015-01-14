6fc758fe906654ebd7f53591868bb04de6d1886a
Shahrukh A. Khan <iam@shaz3e.com>
2015-01-15 03:46:51 +0500

    Google Analytics Updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -110,20 +110,17 @@ if( s3_option('responsive_video') == 1): ?>
 </script>
 <?php endif; ?>    
     
-<?php if( s3_option('google_analytics') ): ?>
-	<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '<?php echo s3_option('google_analytics') ; ?>']);
-	_gaq.push(['_trackPageview']);
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+<?php if( s3_option('google_analytics') ): ?>  
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+  ga('create', '<?php echo s3_option('google_analytics') ; ?>', 'auto');
+  ga('send', 'pageview');
 
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-	</script>
-
+</script>
 <?php endif; ?>
 
 <link rel="shortcut icon" href="<?php echo $dcTemplate; ?>/images/favicon/favicon.ico">
