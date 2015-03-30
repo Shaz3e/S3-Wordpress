328b5b72edba0c48a6d8539d6bc72237418a8bab
Shahrukh A. Khan <iam@shaz3e.com>
2015-03-30 18:02:07 +0500

    Respond.js HTML5Shiv Updated

--- s3tools/s3_head.php
+++ s3tools/s3_head.php
@@ -189,7 +189,7 @@ if( s3_option('responsive_video') == 1): ?>
 	// load html5 library
 	if( s3_option('hosted_cdn') == 1 ){
 		// load html5 library on CDN
-		echo '<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>';
+		echo '<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>';
 	}else{
 		// load html5 library locally
 		echo '<script src="' . esc_url( get_template_directory_uri() ) . '/js/html5.js"></script>';
@@ -198,7 +198,7 @@ if( s3_option('responsive_video') == 1): ?>
 	// load respond library
 	if( s3_option('hosted_cdn') == 1 ){
 		// load respond library on CDN
-		echo '<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>';
+		echo '<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>';
 	}else{
 		// load respond library locally
 		echo '<script src="'. esc_url( get_template_directory_uri() ) . '/js/respond.js"></script>';
