4d54557bdf8244afe7860882472adf8539815353
Shahrukh A. Khan <iam@shaz3e.com>
2016-02-28 02:34:56 +0500

    Full Screen Search Added v2.0

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#search-open").click(function(){
+        $("#s3search").show("fast");
+    });
+    $("#search-close").click(function(){
+        $("#s3search").hide("fast");
+    });
+});
