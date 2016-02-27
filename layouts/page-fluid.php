0ac9abd9c64d3712a448a6d0aaa7efd13239df83
Shahrukh A. Khan <iam@shaz3e.com>
2016-02-28 02:31:55 +0500

    Updated in v2.0

--- layouts/page-fluid.php
+++ layouts/page-fluid.php
@@ -6,58 +6,17 @@
  * @since S3 Framework 1.0
  */
  
+
 // Wordpress Header
-get_header();
+	get_header();
 
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+while ( have_posts() ) : the_post();
 
+	// Include the page content template.
+	get_template_part( 'content', 'page' );					
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				    
-				    <?php if(is_page_template('layouts/no-title.php')):
-						
-						/**
-						 * No title on front page (static) or no-title layout page
-						 *
-						 * @since S3 Framework 1.0
-						 */
-				        
-					else: ?>
-				        
-				        <div class="page-header">
-				            <h1 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-				        </div>
-				        
-				    <?php endif; ?>
-				    
-					<div class="page-content">
-						<?php
-							// page content
-							the_content();
-							
-							/** 
-							 * edit link visisble if user is logged and has permission to edit the page
-							 * 
-							 * @since S3 Framework 1.0
-							 */
-							edit_post_link( __( 'Edit', 'S3Framework' ), '<span class="edit-link">', '</span>' );
-						?>
-					</div><!-- .page-content -->
-				    
-				</div><!-- #post-## -->
-
-
-			</div>
-		<?php endwhile;
-	else :
-	
-		// If no content, include the "No posts found" template.
-		get_template_part( 'content', 'none' );
-
-	endif; // if ( have_posts() ) :
+endwhile;
 
 // Wordpress Footer
-get_footer();
+	get_footer();
 ?>
