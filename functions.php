2cc651fce7afefe95a7bc1f53c0e6d9f261599e2
Shahrukh A. Khan <iam@shaz3e.com>
2016-03-20 19:57:39 +0500

    pagination fixed

--- functions.php
+++ functions.php
@@ -84,61 +84,68 @@ set_post_thumbnail_size(
 );
 
 /**
- * Pagination
+ * Numeric Bootstrap3 Pagination
  * Default pagination function for S3Framework
+ * http://www.wpbeginner.com/wp-themes/how-to-add-numeric-pagination-in-your-wordpress-theme/
+ * http://getbootstrap.com/components/#pagination
  * 
  * @since S3 Framework 1.6.1
  */
 function S3_numeric_posts_nav() {
 	echo '<div class="dc-clear"></div>';
-	if( is_singular() )
-		return;
-	global $wp_query;
-	/** Stop execution if there's only 1 page */
-	if( $wp_query->max_num_pages <= 1 )
-		return;
-	$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
-	$max   = intval( $wp_query->max_num_pages );
-	/**	Add current page to the array */
-	if ( $paged >= 1 )
-		$links[] = $paged;
-	/**	Add the pages around the current page to the array */
-	if ( $paged >= 3 ) {
-		$links[] = $paged - 1;
-		$links[] = $paged - 2;
-	}
-	if ( ( $paged + 2 ) <= $max ) {
-		$links[] = $paged + 2;
-		$links[] = $paged + 1;
-	}
-	echo '<div style="text-align:center !important;"><ul class="pagination">' . "\n";
-	/**	Previous Post Link */
-	if ( get_previous_posts_link() )
-		printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
-	/**	Link to first page, plus ellipses if necessary */
-	if ( ! in_array( 1, $links ) ) {
-		$class = 1 == $paged ? ' class="active"' : '';
-		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
-		if ( ! in_array( 3, $links ) )
-			//echo '<li>…</li>';
-			printf( '<li%s><a href="%s">...</a></li>' . "\n", $class, esc_url( get_pagenum_link( 2 ) ), '2' );
-	}
-	/**	Link to current page, plus 2 pages in either direction if necessary */
-	sort( $links );
-	foreach ( (array) $links as $link ) {
-		$class = $paged == $link ? ' class="active"' : '';
-		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
-	}
-	/**	Link to last page, plus ellipses if necessary */
-	if ( ! in_array( $max, $links ) ) {
-		if ( ! in_array( $max - 1, $links ) )
-			//echo '<li>…</li>' . "\n";
-			printf( '<li%s><a href="%s">...</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max - 1 ) ), $max - 1 );
-		$class = $paged == $max ? ' class="active"' : '';
-		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
-	}
-	/**	Next Post Link */
-	if ( get_next_posts_link() )
-		printf( '<li>%s</li>' . "\n", get_next_posts_link() );
-	echo '</ul></div>' . "\n";
+    if (is_singular()) {
+        return;
+    }
+    global $wp_query;
+    /** Stop execution if there's only 1 page */
+    if ($wp_query->max_num_pages <= 1) {
+        return;
+    }
+    $paged = get_query_var('paged') ? absint(get_query_var('paged')) : 1;
+    $max = intval($wp_query->max_num_pages);
+    /** Add current page to the array */
+    if ($paged >= 1) {
+        $links[] = $paged;
+    }
+    /** Add the pages around the current page to the array */
+    if ($paged >= 3) {
+        $links[] = $paged - 1;
+        $links[] = $paged - 2;
+    }
+    if (($paged + 2) <= $max) {
+        $links[] = $paged + 2;
+        $links[] = $paged + 1;
+    }
+    echo '<div style="text-align:center !important;" class="pagination_wrapper"><ul class="pagination">' . "\n";
+    /** Previous Post Link */
+    if (get_previous_posts_link()) {
+        printf('<li>%s</li>' . "\n", get_previous_posts_link());
+    }
+    /** Link to first page, plus ellipses if necessary */
+    if (!in_array(1, $links)) {
+        $class = 1 == $paged ? ' class="first active"' : ' class="first"';
+        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link(1)), '1');
+        if (!in_array(2, $links)) {
+            echo '<li><span class="btn disabled">…</span></li>' . "\n";
+        }
+    }
+    /** Link to current page, plus 2 pages in either direction if necessary */
+    sort($links);
+    foreach ((array)$links as $link) {
+        $class = $paged == $link ? ' class="last active"' : ' class="last"';
+        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($link)), $link);
+    }
+    /** Link to last page, plus ellipses if necessary */
+    if (!in_array($max, $links)) {
+        if (!in_array($max - 1, $links)) {
+            echo '<li><span class="btn disabled">…</span></li>' . "\n";
+        }
+        $class = $paged == $max ? ' class="active"' : '';
+        printf('<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($max)), $max);
+    }
+    /** Next Post Link */
+    if (get_next_posts_link()) {
+        printf('<li>%s</li>' . "\n", get_next_posts_link());
+    }
+    echo '</ul></div>' . "\n";
 }
