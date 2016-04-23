8321f3c17eddc59ca9c1548759c0e327401ce05f
Shahrukh A. Khan <iam@shaz3e.com>
2016-04-24 00:59:09 +0500

    Params file updated

--- functions/s3_css_js.php
+++ functions/s3_css_js.php
@@ -22,99 +22,88 @@
 
 function s3_css_js(){
 
-	// Load js Files with CDN
-	if( s3_option('hosted_cdn') == 1 ){
+	/**
+	 * Load Files from CDN or Locally
+	 */
+	if ( s3_option('LocalCDN') == 1){
+
+		/**
+		 * Load Files from CDN
+		 */
+	 
+		// Load Font Awesome
+		if( s3_option('loadFontAwesome') == 1){
+			wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css', array(), '4.6.1' );
+		}
+		
+		// Load Boostrap
+		if( s3_option('loadBootstrap') == 1 ){
+			wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', array(), '3.3.6' );
+		}
+		
+		// Load Bootstrap Theme
+		if( s3_option('loadBootstrapTheme') == 1 ){
+			wp_enqueue_style( 'bootstrap-theme', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css', array(), '3.3.6' );
+		}
+		
+		// Load animate.css
+		if( s3_option('loadAnimateCSS') == 1 ){
+			wp_enqueue_style( 'animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css', array(), '3.5.1' );
+		}
+		
+		// Load jQuery
 		wp_enqueue_script( 'jquery.min', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js', array(), '2.2.0' );	
 		
-		// load bootstrap library
-		if( s3_option('bootstrap')  == 1){
-			wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array(), '3.3.5' );
+		// Load Bootstrap
+		if( s3_option('loadBootstrap') == 1 ){
+			wp_enqueue_script( 'jquery-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array(), '3.3.6' );
+		}
+		
+		// jQuery FitVIDS
+		if( s3_option('loadResponsiveVideo') == 1 ){
+			wp_enqueue_script( 'jquery-fitvids', '//cdnjs.cloudflare.com/ajax/libs/fitvids/1.1.0/jquery.fitvids.min.js', array(), '1.1' );
 		}
 
-		// load Responsive Video fitvids.js with CDN
-		if( s3_option('responsive_video') == 1):
-			wp_enqueue_script( 'fitvids', '//cdnjs.cloudflare.com/ajax/libs/fitvids/1.1.0/jquery.fitvids.min.js', array(), '1.1' );
-		endif;
-		
-	// Load js File Locally
 	}else{
+		/**
+		 * Load Files Locally
+		 */
 		
-		wp_enqueue_script( 'jquery.min', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.11.3' );
-		
-		// load bootstrap library
-		if( s3_option('bootstrap')  == 1){
-			wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.6' );
+		// Load Font Awesome
+		if( s3_option('loadFontAwesome') == 1 ){
+			wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/font-awesome/css/font-awesome.min.css', array(), '4.6.1' );
 		}
 		
-		// load Responsive Video fitvids.js localy
-		if( s3_option('responsive_video') == 1):
-			wp_enqueue_script( 'fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array(), '1.1.0' );
-		endif;
-	}
-
-	// load S3 Framework Responsive Menu
-	wp_enqueue_script( 's3-menu', get_template_directory_uri() . '/js/menu.js', array(), '1.0' );
-	
-	// load user script before closing </body> tag
-	wp_enqueue_script( 's3-script', get_template_directory_uri() . '/js/search.js', array(), '1.0' );
-
-	// load user script before closing </body> tag
-	wp_enqueue_script( 's3-script', get_template_directory_uri() . '/js/scripts.js', array(), '1.0' );
-	
-	/**
-	 * Load CSS Files
-	 * Files are Hosted Localy or CDN
-	 * 
-	 */
-	
-	// load font awesome library
-	if( s3_option('font_awesome')  == 1){
-		// load font awesome library on CDN
-		if( s3_option('hosted_cdn') == 1 ){
-			wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), '4.5.0' );
-		}else{
-		// load font awesome library locally
-			wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/font-awesome/css/font-awesome.min.css', array(), '4.5.0' );
-		}
-	}
-	
-	// load bootstrap library
-	if( s3_option('bootstrap')  == 1){
-		// load bootstrap on CDN
-		if( s3_option('hosted_cdn') == 1 ){
-			wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', array(), '3.3.6' );
-		}else{
-			// load bootstrap locally
+		// Load Bootstrap
+		if( s3_option('loadBootstrap') == 1 ){
 			wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '3.3.6' );
 		}
-	}
-	
-	// load bootstrap theme library
-	if( s3_option('bootstrap_theme')  == 1){
-		// load bootstrap theme on CDN
-		if( s3_option('hosted_cdn') == 1 ){
-			wp_enqueue_style( 'bootstrap-theme', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css', array(), '3.3.6' );
-		}else{
-			// load bootstrap theme locally
+		
+		// Load Bootstrap Theme
+		if( s3_option('loadBootstrapTheme') == 1 ){
 			wp_enqueue_style( 'bootstrap-theme', get_template_directory_uri() . '/css/bootstrap-theme.css', array(), '3.3.6' );
 		}
-	}
-	
-	/**
-	 * Load animate.css 
-	 * 
-	 * @since S3 Framework 2.0
-	 */
-	if( s3_option('animate_css')  == 1){
-		// load animate.css on CDN
-		if( s3_option('hosted_cdn') == 1 ){
-			wp_enqueue_style( 'animate', 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css', array(), '3.5.1' );
-		}else{
-			// load animate.css locally
+		
+		// Load animate.css
+		if( s3_option('loadAnimateCSS') == 1 ){
 			wp_enqueue_style( 'animate', get_template_directory_uri() . '/css/animate.css', array(), '3.5.1' );
+		}	
+		
+		// Add Javascripts
+		wp_enqueue_script( 'jquery.min', get_template_directory_uri() . '/js/jquery.min.js', array(), '1.11.3' );
+		
+		
+		// Load Bootstrap
+		if( s3_option('loadBootstrap') == 1 ){
+			wp_enqueue_script( 'jquery-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.6' );
+		}
+		
+		// jQuery FitVIDS
+		if( s3_option('loadResponsiveVideo') == 1 ){
+			wp_enqueue_script( 'jquery-fitvids', get_template_directory_uri() . '/js/jquery.fitvids.js', array(), '1.1.0' );
 		}
 	}
-	
+
 	/**
 	 * load user fonts only when google fonts are disabled
 	 * Google Fonts Added
@@ -124,6 +113,16 @@ function s3_css_js(){
 	if( s3_option('gfont_config') == 0 ):
 		wp_enqueue_style( 's3-fonts', get_template_directory_uri() . '/fonts/fonts.css', array(), '1.0' );
 	endif;
+
+
+	// load S3 Framework Responsive Menu
+	wp_enqueue_script( 's3-menu', get_template_directory_uri() . '/js/menu.js', array(), '1.0' );
+	
+	// load user script before closing </body> tag
+	wp_enqueue_script( 's3-script', get_template_directory_uri() . '/js/search.js', array(), '1.0' );
+
+	// load user script before closing </body> tag
+	wp_enqueue_script( 's3-script', get_template_directory_uri() . '/js/scripts.js', array(), '1.0' );
 	
 	/**
 	 * Calls less.js and style.less files in s3tools/s3_head.php
