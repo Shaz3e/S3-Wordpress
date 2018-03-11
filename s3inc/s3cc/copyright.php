c386b339868048c87517b5ae765650ce35d4857f
Shahrukh A. Khan <iam@shaz3e.com>
2018-03-11 23:00:25 +0500

    fixed typo

--- s3inc/s3cc/copyright.php
+++ s3inc/s3cc/copyright.php
@@ -5,7 +5,7 @@
 		}elseif( s3_option('copyright_text') ){
 			echo '<p>'.s3_option('copyright_text').'</p>';
 		}else{ ?>
-			<p>&copy; <?php echo date('Y'); ?> <a href="<?php echo bloginfo('url'); ?>" title="<?php echo( s3_option('sitename') == '' ? bloginfo('name') : s3_option('sitename') );?>"><?php echo( s3_option('sitename') == '' ? bloginfo('name') : s3_option('sitename') );?></a> | All Rights Reserved.</p>
+			<p>All &copy; <?php echo date('Y'); ?> <a href="<?php echo bloginfo('url'); ?>" title="<?php echo( s3_option('sitename') == '' ? bloginfo('name') : s3_option('sitename') );?>"><?php echo( s3_option('sitename') == '' ? bloginfo('name') : s3_option('sitename') );?></a> | All Rights Reserved.</p>
 		<?php }
 	}
 ?>
