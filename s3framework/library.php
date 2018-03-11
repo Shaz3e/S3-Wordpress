3d40fa7e16b79b7927f264181df2c6ed18d1f6c5
Shahrukh A. Khan <iam@shaz3e.com>
2018-03-11 23:00:38 +0500

    New library option added

--- s3framework/library.php
+++ s3framework/library.php
@@ -38,18 +38,6 @@
 			'section' => 'library'
 		);
 		
-		$this->settings['loadBootstrapTheme'] = array(
-			'title'   => __( 'Use Bootstrap Theme' ),
-			'desc'    => __( 'Enable or Disable Bootstrap Theme' ),
-			'std'     => 0,
-			'type'    => 'select',
-			'choices' => array(
-							1 => 'Yes',
-							0 => 'No',
-						 ),
-			'section' => 'library'
-		);
-		
 		$this->settings['loadFontAwesome'] = array(
 			'title'   => __( 'Use Font Awesome' ),
 			'desc'    => __( 'Enable or Disable Font Awesome for Icons' ),
@@ -77,6 +65,30 @@
 		$this->settings['loadResponsiveVideo'] = array(
 			'title'   => __( 'Use Responsive Video' ),
 			'desc'    => __( 'Enable this library to use responsive video. Example: <code>&lt;div class="s3-video"&gt;your-video-code&lt;/div&gt;</code>' ),
+			'std'     => 0,
+			'type'    => 'select',
+			'choices' => array(
+							1 => 'Yes',
+							0 => 'No',
+						 ),
+			'section' => 'library'
+		);
+
+		$this->settings['loadResponsiveText'] = array(
+			'title'   => __( 'Use jQuery FitText' ),
+			'desc'    => __( 'FitText makes font-sizes flexible. Use this plugin on your responsive design for ratio-based resizing of your headlines.<br> Example: <code>jQuery("#responsive_headline").fitText();</code><br>More information at www.fittextjs.com<br><strong>Note: use jquery in theme_folder/js/scripts.js</strong>' ),
+			'std'     => 0,
+			'type'    => 'select',
+			'choices' => array(
+							1 => 'Yes',
+							0 => 'No',
+						 ),
+			'section' => 'library'
+		);
+
+		$this->settings['loadLettering'] = array(
+			'title'   => __( 'Use jQuery Lettering' ),
+			'desc'    => __( 'a jQuery plugin for radical Web Typography.<br> HTML Markup: <code>&lt;h1 class="fancy_title">Some Title&lt;/h1&gt;</code><br>jQuery:<br>$(document).ready(function() {<br>&nbsp;&nbsp;&nbsp;&nbsp;$(".fancy_title").lettering();<br>});<br>More information at www.letteringjs.com<br><strong>Note: use jquery in theme_folder/js/scripts.js</strong>' ),
 			'std'     => 0,
 			'type'    => 'select',
 			'choices' => array(
