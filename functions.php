d7c87e031c0be04e632c25242429a61d4b455023
Shahrukh A. Khan <iam@shaz3e.com>
2020-10-31 12:42:44 +0500

    WebP Image Support added

--- functions.php
+++ functions.php
@@ -106,3 +106,36 @@ function S3_numeric_posts_nav() {
         ));
     }
 }
+
+/**
+ * WebP Image Support added
+ * @since S3 Framework 1.6.1
+ * @updated S3 Framework 1.6.2
+ * @credit https://mariushosting.com/how-to-upload-webp-files-on-wordpress/
+ */
+
+//** *Enable upload for webp image files.*/
+function webp_upload_mimes($existing_mimes) {
+    $existing_mimes['webp'] = 'image/webp';
+    return $existing_mimes;
+}
+add_filter('mime_types', 'webp_upload_mimes');
+
+//** * Enable preview / thumbnail for webp image files.*/
+function webp_is_displayable($result, $path) {
+    if ($result === false) {
+        $displayable_image_types = array( IMAGETYPE_WEBP );
+        $info = @getimagesize( $path );
+
+        if (empty($info)) {
+            $result = false;
+        } elseif (!in_array($info[2], $displayable_image_types)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+    }
+
+    return $result;
+}
+add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
