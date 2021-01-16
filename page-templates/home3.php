64a9807f2581e117a7eadd2b9606494e5e00c872
Shaz3e <iam@shaz3e.com>
2021-01-16 16:18:15 +0500

    page style and template added

---
+++ page-templates/home3.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Template Name: Home 3
+ * custome page template for dummy
+ * @since S3 Framework 1.0
+ */
+ 
+// Wordpress Header
+	get_header();
+
+// modify this page according your requirements make sure to copy first before use
+
+//while ( have_posts() ) : the_post();
+
+	// Include the page content template.
+	//get_template_part( 'content', 'page' );
+	
+	// If comments are open or we have at least one comment, load up the comment template.
+	//if ( comments_open() || get_comments_number() ) {
+		//comments_template();
+	//}
+					
+
+//endwhile;
+
+// Wordpress Footer
+	get_footer();
+?>
