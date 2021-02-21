fa076d47a8d68f7daedb3a1632ccee4d78210044
Shaz3e <iam@shaz3e.com>
2021-02-21 21:19:02 +0500

    Update

--- s3inc/headers/header-0.php
+++ s3inc/headers/header-0.php
@@ -1,9 +1,9 @@
 <?php
-	/**
-	 * Header Styles - Default Header
-	 * File Name: header-0.php
-	 * @since S3 Framework 4.0
-	 */
+    /**
+     * Header Styles - Default Header
+     * File Name: header-0.php
+     * @since S3 Framework 4.0
+     */
 ?>
 <div class="dc-fixed-header">
     <section class="dc-header dc-header-<?php echo s3_option('header_style'); ?> dc-clear" id="dc-header">
@@ -11,29 +11,29 @@
             <div class="s3-grid-<?php echo s3_option('logo_column'); ?>">
                 <div class="block">
                     <div class="dc-logo">
-                    	<?php echo s3_logo(); ?>
+                        <?php echo s3_logo(); ?>
                     </div>
                 </div>
             </div><?php // .s3-grid-3 ?>
             
             <?php if( is_s3_headers() ): ?>
             <div class="s3-grid<?php echo s3_option('logo_column') - 12; ?>">
-            	<div class="block">
+                <div class="block">
                     <div class="dc-header-boxes">
-						<?php phone_text(); ?>
+                        <?php phone_text(); ?>
                         <?php email_text();?>
                         <?php social_icons();?>
                         <?php search_box();?>
                     </div>
                     <?php calltoaction();?>
-				</div>
+                </div>
             </div>
             <?php endif; ?>
         </div><?php // .row ?>
         
         <div class="<?php echo( s3_option('fluidContainer') == 1 ? 'container-fluid' : 'row' ); ?>">
-        	<div class="s3-col-1">
-                <div class="block">
+            <div class="s3-col-1">
+                <div class="block dc-clear">
                     <?php s3_main_menu(); ?>
                 </div>
             </div>
